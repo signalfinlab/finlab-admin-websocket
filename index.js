8a31f471f3021c073f14825277ba7da41434f156
박경준 <pkj6921@naver.com>
2023-06-26 11:17:41 +0900

    pusher 세팅

---
+++ index.js
@@ -0,0 +1,13 @@
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: '1624709',
+  key: 'a6ef665a2ac6b4aaf716',
+  secret: 'e329e25488bc234a1c0c',
+  cluster: 'ap3',
+  useTLS: true,
+});
+
+pusher.trigger('my-channel', 'my-event', {
+  message: 'hello world',
+});
